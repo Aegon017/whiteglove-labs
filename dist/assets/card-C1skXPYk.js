f617df84d30e635fa45dc9347e371b0b6317d78d
Aegon017 <kondanagamalleswararao016@gmail.com>
2025-09-09 12:52:33 +0530

    changes are done

---
+++ dist/assets/card-C1skXPYk.js
@@ -0,0 +1,6 @@
+import{c as s,j as t,a as e}from"./index-ieaTw34_.js";/**
+ * @license lucide-react v0.542.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const d=[["path",{d:"M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2",key:"1yyitq"}],["path",{d:"M16 3.128a4 4 0 0 1 0 7.744",key:"16gr8j"}],["path",{d:"M22 21v-2a4 4 0 0 0-3-3.87",key:"kshegd"}],["circle",{cx:"9",cy:"7",r:"4",key:"nufk8"}]],c=s("users",d);function n({className:a,...r}){return t.jsx("div",{"data-slot":"card",className:e("bg-card text-card-foreground flex flex-col gap-6 rounded-xl border py-6 shadow-sm",a),...r})}function i({className:a,...r}){return t.jsx("div",{"data-slot":"card-header",className:e("@container/card-header grid auto-rows-min grid-rows-[auto_auto] items-start gap-1.5 px-6 has-data-[slot=card-action]:grid-cols-[1fr_auto] [.border-b]:pb-6",a),...r})}function l({className:a,...r}){return t.jsx("div",{"data-slot":"card-title",className:e("leading-none font-semibold",a),...r})}function u({className:a,...r}){return t.jsx("div",{"data-slot":"card-content",className:e("px-6",a),...r})}export{n as C,c as U,u as a,i as b,l as c};
---
+++ dist/assets/info-card-DrKmPVBT.js
@@ -0,0 +1,11 @@
+import{c as s,j as e}from"./index-ieaTw34_.js";import{C as o,a as n}from"./card-C1skXPYk.js";/**
+ * @license lucide-react v0.542.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const d=[["path",{d:"M3 3v16a2 2 0 0 0 2 2h16",key:"c24i48"}],["path",{d:"M18 17V9",key:"2bz60n"}],["path",{d:"M13 17V5",key:"1frdt8"}],["path",{d:"M8 17v-3",key:"17ska0"}]],i=s("chart-column",d);/**
+ * @license lucide-react v0.542.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const l=[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["circle",{cx:"12",cy:"12",r:"6",key:"1vlfrh"}],["circle",{cx:"12",cy:"12",r:"2",key:"1c9p78"}]],x=s("target",l),p=({icon:a,title:c,description:t})=>{const r=a;return e.jsx(o,{className:"h-full border-border bg-card shadow-sm hover:shadow-md transition-shadow",children:e.jsxs(n,{className:"p-6",children:[e.jsxs("div",{className:"flex gap-2 mb-3",children:[r&&e.jsx(r,{className:"w-6 h-6 text-foreground"}),e.jsx("h3",{className:"text-lg font-semibold text-card-foreground",children:c})]}),e.jsx("p",{className:"mt-2 text-sm text-muted-foreground",children:t})]})})};export{i as C,p as I,x as T};
